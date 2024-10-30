770a54bea78e4baa8334625b6367e289bc1d247d
Sarvamangala Yathada <YSarvamangala@Opteamix.com>
2024-10-30 20:40:57 +0530

    removed maps from sidebar

--- src/components/Layout/sidebar/SidebarData.tsx
+++ src/components/Layout/sidebar/SidebarData.tsx
@@ -23,13 +23,6 @@ export const SidebarData = [
     icon: <span className="fa-stack fa-md"><i className="fa fa fa-users fa-stack-1x "></i></span>
   },
   {
-    title: 'Maps',
-    id: 'maps',
-    path: '/landing-page/maps',
-    scopes: ['Admin'],
-    icon: <span className="fa-stack fa-md"></span>
-  },
-  {
     title: 'Products',
     id: 'product',
     path: '/landing-page/products',
