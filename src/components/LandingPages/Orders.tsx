60bdebf077059450c7dbf21437a455a10f0ba1f1
Sarvamangala Yathada <YSarvamangala@Opteamix.com>
2024-12-20 17:10:49 +0530

    search icon css fixes

--- src/components/LandingPages/Orders.tsx
+++ src/components/LandingPages/Orders.tsx
@@ -475,14 +475,11 @@ const Orders = () => {
                                                 }
                                         </div>
 
-                                        <input className="search_input category-selector-search-input" type="text" name="" value={searchString} placeholder="search here" 
-                                        onChange={handleSearchStringChange} onKeyDown={handleEnterPressForSearch}/>
-                                        <button id="searchQuerySubmit" type="submit" name="searchQuerySubmit" onClick={()=>setApplyfilter(true)}>
-                                            <svg style={{ width: '24px', height: '24px' }} viewBox="0 0 24 24">
-                                                <path fill="#666666"
-                                                    d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
-                                            </svg>
-                                        </button>
+                                        <div className="search d-flex">
+                                            <input className="search_input" type="text" name="" value={searchString} placeholder="search here" 
+                                            onChange={handleSearchStringChange} onKeyDown={handleEnterPressForSearch}/>
+                                            <span className="search_icon cursor-pointer" onClick={()=>setApplyfilter(true)}><i className="fa fa-search"></i></span>
+                                        </div>
                                         {isOpen && (
                                                 <div className="search-by-selection-dropdown">
                                                     {
